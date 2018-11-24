885b8d78df61b22131bb68d09df040fd94a5ea21
Mr.Z <zrd0921@qq.com>
2018-11-24 11:11:28 +0800

    update

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,6 +1,8 @@
+/**
+ * 判断浏览器是否支持passive, 默认preventDefault无效
+ */
 export const isPassiveSupported = (() => {
 	let status = false;
-	// 判断浏览器是否支持passive, 默认preventDefault无效
 	try {
 		let opts = Object.defineProperty({}, "passive", {
 			get() {
