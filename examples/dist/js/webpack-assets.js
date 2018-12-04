43ea89b6639c6d2fbfcbf107695a073864c42e56
Mr.Z <zrd0921@qq.com>
2018-12-04 21:38:10 +0800

    update:demo

--- examples/dist/js/webpack-assets.js
+++ examples/dist/js/webpack-assets.js
@@ -1,1 +1,1 @@
-window.WEBPACK_ASSETS={"common":{"js":"/wya-vm/examples/dist/js/common.7dd26ed8.chunk.js"},"main":{"js":"/wya-vm/examples/dist/js/main.7dd26ed8.bundle.js","css":"/wya-vm/examples/dist/css/initial.main.7dd26ed8.css"}}
+window.WEBPACK_ASSETS={"common":{"js":"/wya-vm/examples/dist/js/common.bf0e00ff.chunk.js"},"main":{"js":"/wya-vm/examples/dist/js/main.bf0e00ff.bundle.js","css":"/wya-vm/examples/dist/css/initial.main.bf0e00ff.css"}}
