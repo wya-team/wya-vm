022eaf57eb936fa034b423441cb2b6929f9ac65e
Mr.Z <zrd0921@qq.com>
2019-06-07 18:55:52 +0800

    update:demo

---
+++ examples/dist/js/webpack-assets.js
@@ -0,0 +1,1 @@
+window.WEBPACK_ASSETS={"common":{"js":"/wya-vm/examples/dist/js/common.865d1228.chunk.js"},"main":{"css":"/wya-vm/examples/dist/css/initial.main.865d1228.css","js":"/wya-vm/examples/dist/js/main.865d1228.bundle.js"}}
