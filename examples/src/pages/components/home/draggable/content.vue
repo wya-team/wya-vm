21718aece8ab39f70e3124db8e7ac3c8431a9147
Mr.Z <zrd0921@qq.com>
2019-10-31 15:01:54 +0800

    test: demo

--- examples/src/pages/components/home/draggable/content.vue
+++ examples/src/pages/components/home/draggable/content.vue
@@ -1,16 +1,22 @@
 <template>
-	<div>
-		<div style="position: absolute; top: 0;" @click="theme = theme === 'dark' ? 'light' : 'dark'">
-			主题: {{ theme }} (点我切换)
-		</div>
+	<div style="padding-top: 20px">
+		<div style="position: absolute; top: 5px;">
+			<vc-button @click="theme = theme === 'dark' ? 'light' : 'dark'">
+				主题: {{ theme }}
+			</vc-button>
 
-		<div style="position: absolute; top: 0; left: 200px" @click="showRuler = !showRuler">
-			标尺: {{ showRuler ? '展示' : '隐藏' }} (点我切换)
+			<vc-button @click="showRuler = !showRuler">
+				标尺: {{ showRuler ? '展示' : '隐藏' }}
+			</vc-button>
+			<vc-button @click="$refs.combo.preview()">
+				预览
+			</vc-button>
 		</div>
 
 		<vm-combo 
+			ref="combo"
 			v-model="list"
-			:frame-style="{ border: '1px solid #5495f6', background: 'white' }"
+			:frame-style="{ background: 'white' }"
 			:frame-w="1920"
 			:frame-h="1080"
 			:width="style.width"
@@ -46,7 +52,20 @@ export default {
 					module: 'page',
 					w: window.screen.width,
 					h: window.screen.height,
+				},
+				{
+					"w": 200,
+					"h": 150,
+					"r": 0,
+					"x": 345,
+					"y": 208,
+					"z": 1,
+					"name": "样式1",
+					"bg": "#E6E8FF",
+					"module": "echart",
+					"id": "vm-15724918705191"
 				}
+
 			],
 			style: {
 				width: window.innerWidth - 40,
@@ -70,10 +89,8 @@ export default {
 			// 需要减去padding值
 			this.style = {
 				width: window.innerWidth - 40,
-				height: window.innerHeight - 40,
+				height: window.innerHeight - 60,
 			};
-
-			console.log(window.innerWidth, window.innerHeight);
 		},
 		handleSave(response) {
 			console.log(response, this.list);
